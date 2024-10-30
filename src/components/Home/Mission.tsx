dda65196f69f04051f45d4a34c36a473e6ab39c9
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2024-10-30 18:27:47 +0100

    feat: sleepStiq landing page

--- src/components/Home/Mission.tsx
+++ src/components/Home/Mission.tsx
@@ -6,7 +6,7 @@ const Mission = () => {
       <section className="xl:flex justify-between items-center">
         <div className="mb-5 xl:mb-0">
           <h3 className="font-bold text-xl sm:text-3xl">Our Mission</h3>
-          <p className="text-fadedPrimary my-6 text-sm sm:w-[70%]">
+          <p className="text-fadedPrimary my-6 text-sm sm:w-[90%]">
             We started Sleepstiq with 1 simple goal: to be your best friend at
             bedtime. We, just like you, deal with stress, unease, and trouble
             sleeping from a number of silly things like school, work, screens,
